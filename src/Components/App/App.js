b0e0853b0b840e84ba6b0eb552bfcbb80f9650b9
GabeG1 <thegabe6@yahoo.com>
2020-07-10 20:29:15 -0500

    phase 1

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render()
+  {
+  return (
+    <div className="App">
+      <h1>Let's do this</h1>
+    </div>
+  );
+}
+}
+export default App;
