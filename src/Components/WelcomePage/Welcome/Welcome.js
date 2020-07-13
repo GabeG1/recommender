a3d481f72df857fbd57208381179945b807caf7f
GabeG1 <thegabe6@yahoo.com>
2020-07-13 18:42:07 -0500

    Welcome Page changes

--- src/Components/WelcomePage/Welcome/Welcome.js
+++ src/Components/WelcomePage/Welcome/Welcome.js
@@ -1,21 +1,22 @@
+
 import React from 'react';
 import './Welcome.css';
 import {DisplayImages} from '../DisplayImages/DisplayImages';
-import {SearchBar} from '../SearchBar/SearchBar';
-import Button from '@material-ui/core/Button/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-//import '~/Downloads/material-kit-react-master/material-kit-react-master/dist/material-kit-react.css';
-//import Button from ' ~/Downloads/material-kit-react-master/material-kit-react-master/dist/components/CustomButtons/Button';
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 
 
 const images = {
-  image_1: "https://images.pexels.com/photos/6270/woman-flowers-holidays-girl-6270.jpg?auto=compress&cs=tinysrgb&h=750&w=1260",
-  image_2: "https://images.pexels.com/photos/1188470/pexels-photo-1188470.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-  image_3: "https://images.pexels.com/photos/115001/pexels-photo-115001.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+  image_1: "https://images.pexels.com/photos/2098428/pexels-photo-2098428.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  image_2: "https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
+  image_3: "https://images.pexels.com/photos/390051/surfer-wave-sunset-the-indian-ocean-390051.jpeg?auto=compress&cs=tinysrgb&h=650&w=940",
   image_4: "https://images.pexels.com/photos/3004075/pexels-photo-3004075.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
-  image_5: "https://images.pexels.com/photos/4009409/pexels-photo-4009409.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  image_5: "https://images.pexels.com/photos/1652353/pexels-photo-1652353.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
 }
+
+const useStyles = makeStyles();
 class Welcome extends React.Component {
   constructor(props) {
     super(props);
@@ -35,23 +36,54 @@ class Welcome extends React.Component {
     });
   }
   render() {
+    const classes = this.props.classes;
    //<CustomLinearProgress variant="determinate" color="primary" value={30}/>
   return (
     <div className="welcomeTitle">
       <link href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre&display=swap" rel="stylesheet"></link>
-      <header>recomen<span>derrr</span>
-    <ButtonGroup size='large' classes={{root: 'buttonGroup'}} disableElevation variant="contained" color="primary">
-  <Button classes={{root: 'accountButtons'}} >Sign In</Button>
-  <Button classes={{root: 'accountButtons'}}>Create Account</Button>
-</ButtonGroup>
-      </header>
-      <DisplayImages updateImage={this.updateImage} image={this.state.imageDisplay}/>
-      <section className="searchBar">
-      <SearchBar />
-      </section>
-     
-    </div>
+    <header>
+      <Grid container direction="row"
+  justify="space-between"
+  alignItems="center">
+        <Grid item className="title">
+      recommen<span id="inlineTitleDesign">derrr</span>
+        </Grid>
+            <Grid item className="signupButtons">
+               <Button
+                 className='loginButton'
+                 onClick={e => e.preventDefault()}
+                 color="white"
+               >
+                Login
+               </Button>
+               </Grid>
+               </Grid>
+               </header>
+          <section className="images">
+     <DisplayImages updateImage={this.updateImage} image={this.state.imageDisplay}>
+    
+     </DisplayImages>
+     </section>
+     <Button
+                 className='signupButton'
+                 onClick={e => e.preventDefault()}
+                 round
+                 size="small"
+                 color="danger"
+               >
+                 Signup
+               </Button>
+  
+      </div>
   );
 }
+
 }
-export default Welcome;
+
+export default () => {
+  const classes = useStyles();
+  return (
+      <Welcome classes={classes} />
+  )
+}
+
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Welcome from './Components/WelcomePage/Welcome/Welcome';
-import Login from './Components/LoginPage/Login/Login';
+import Login from './Components/LoginPage/Login/Login.js';
+
 
 ReactDOM.render(<Welcome />, document.getElementById('root'));
 
